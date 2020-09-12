448964bb50b414349ac8023990210073fc5e15fd
ramimhossain12 <ramimhossain6242@gmail.com>
2020-09-12 23:49:34 +0600

    kotlin

---
+++ app/src/main/java/com/example/proguardforsourcecodesecurity/a/MainActivity2.kt
@@ -0,0 +1,12 @@
+package com.example.proguardforsourcecodesecurity.a
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.proguardforsourcecodesecurity.R
+
+class MainActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+    }
+}
